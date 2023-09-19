bf8dff24b01a69e0437b216cd87f48c9911e0c2f
Beytio <oguzbeytullah@gmail.com>
2023-09-19 23:10:18 +0300

    object class

---
+++ src/day22_ClassAndObject_MemoryInJava/Math2.java
@@ -0,0 +1,11 @@
+package day22_ClassAndObject_MemoryInJava;
+
+public class Math2 {
+
+        public  int pow(int x,int y){
+            for (int i = 0; i < y; i++) {
+                x*=x;
+            }
+            return x;
+
+}}
