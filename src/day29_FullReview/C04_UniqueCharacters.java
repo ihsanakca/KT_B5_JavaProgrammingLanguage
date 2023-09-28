255dea30a1fc23a2c1cb5bea4d03fac818857265
ihsan <111647146+nested4@users.noreply.github.com>
2023-09-28 21:29:17 +0300

    day29 added

---
+++ src/day29_FullReview/C04_UniqueCharacters.java
@@ -0,0 +1,17 @@
+package day29_FullReview;
+
+public class C04_UniqueCharacters {
+
+}
+/*Write a method that can return the unique characters from a String
+
+			Ex:
+				input:
+					AABCCD
+
+				output:
+					BD
+
+			Hint: You will need indexOf() and lastIndexOf()
+				  if the first and last index numbers of the character are same, then it's unique
+ */
