9aacbda34ec393989f6cce34a529b93294192e88
ihsan <111647146+nested4@users.noreply.github.com>
2023-10-19 21:16:22 +0300

    day44 added

---
+++ src/day39_ConstructorOverloading_Encapsulation/C05_EmployeeObjects.java
@@ -0,0 +1,11 @@
+package day39_ConstructorOverloading_Encapsulation;
+
+public class C05_EmployeeObjects {
+    public static void main(String[] args) {
+
+//        Employee employee1 = new Employee("Ahmet",'c',33,45000.98);
+//        System.out.println(employee1);
+        Employee employee2 = new Employee("Ahmet",'f',-15,45000.98);
+        System.out.println(employee2);
+    }
+}
