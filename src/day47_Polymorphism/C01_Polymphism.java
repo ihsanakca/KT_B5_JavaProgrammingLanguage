bcc86da409fda5e3ec03a6d247f5117f4afdc30e
ihsan <111647146+nested4@users.noreply.github.com>
2023-10-24 22:22:47 +0300

    day 47 added

---
+++ src/day47_Polymorphism/C01_Polymphism.java
@@ -0,0 +1,46 @@
+package day47_Polymorphism;
+
+import day44_AbstracClassAndInterface.animal.Animal;
+import day44_AbstracClassAndInterface.animal.Cat;
+import day44_AbstracClassAndInterface.animal.Dog;
+
+public class C01_Polymphism {
+    public static void main(String[] args) {
+
+
+        Animal animal = new Dog("Panco", "Golden", 'm', 10, "Medium", "Gold"); //polymorhism
+        Dog dog = new Dog("Lucky", "Husky", 'f', 7, "Large", "White");
+
+        boolean isAnimal = animal instanceof Animal;
+        System.out.println(isAnimal);
+        boolean isDog = animal instanceof Dog;
+        System.out.println(isDog);
+        boolean isCat = animal instanceof Cat;
+        System.out.println(isCat);
+
+        System.out.println("--------------------------");
+        animal.eat();
+        animal.drinkWater();
+
+        System.out.println("--------------------------");
+        ((Dog)animal).bark();// Explicit casting downcasting
+        ((Dog)animal).play();
+
+        animal = new Cat("Panco", "Golden", 'm', 10, "Medium", "Gold");
+        animal.eat();
+        ((Cat)animal).meow();
+
+        Cat cat = new Cat("Bulut","Scottish",'m',4,"Medium","Gray");
+        Animal animal1 = cat;
+        System.out.println(animal1);
+        animal1.eat();
+        animal1.drinkWater();
+        System.out.println(animal1.getBreed());
+
+        System.out.println(animal1 instanceof Cat);
+        System.out.println(animal1 instanceof Animal);
+
+
+    }
+
+}
---
+++ src/day47_Polymorphism/shape/Square.java
@@ -0,0 +1,40 @@
+package day47_Polymorphism.shape;
+
+public class Square extends Shape {
+
+    private double side;
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        if(side<0){
+            System.err.println("Invalid Side Length "+side);
+        }else {
+            this.side=side;
+        }
+    }
+
+    public Square(double side) {
+        super("Square");
+       setSide(side);
+    }
+
+    @Override
+    public double area() {
+        return side*side;
+    }
+
+    @Override
+    public double perimeter() {
+        return 4*side;
+    }
+
+    @Override
+    public String toString() {
+        return "Square{" +
+                "side=" + side +
+                "} " + super.toString();
+    }
+}
